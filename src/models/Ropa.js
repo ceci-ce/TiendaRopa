42c9465e8bdf95dd0cc1fa60fc4f727b85c22c2a
Cecilia <cecilia.delacamara@gmail.com>
2026-03-07 19:23:49 +0100

    feat: update package.json to use src/server.js and add new Ropa model fields

--- src/models/Ropa.js
+++ src/models/Ropa.js
@@ -8,7 +8,15 @@ const ropaSchema = new mongoose.Schema({
     precio: {
         type: Number,
         required: true
-    }
+    },
+    talla: {
+        type: String,
+        default: "M",
+    },
+    stockDisponible: {
+        type: Number,
+        default: 10,
+    },
 });
 
 module.exports = mongoose.model("Ropa", ropaSchema);
